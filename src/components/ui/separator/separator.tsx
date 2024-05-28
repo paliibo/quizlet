179bc044125d608362417fa49789cf08c06d6c8b
iluaboltov <bogdan.paly1@gmail.com>
2024-05-28 09:25:42 +0300

    feat: Added Separator Component

---
+++ src/components/ui/separator/separator.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
+
+export const Separator = forwardRef<
+  ElementRef<typeof SeparatorPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(({ className, decorative = true, orientation = "horizontal", ...props }, ref) => (
+  <SeparatorPrimitive.Root
+    className={"bg-border h-[1px] w-full shrink-0 rounded-lg bg-gray-300"}
+    decorative={decorative}
+    orientation={orientation}
+    ref={ref}
+    {...props}
+  />
+));
+Separator.displayName = "Separator";
