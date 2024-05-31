f34a71c5a3bc262cf78c9b6c77db8fb02fe8293c
iluaboltov <bogdan.paly1@gmail.com>
2024-05-31 16:36:04 +0300

    feat: New Progress Bar

--- src/components/ui/progress/progress.tsx
+++ src/components/ui/progress/progress.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import * as ProgressPrimitive from "@radix-ui/react-progress";
+import { Indicator, Root } from "@radix-ui/react-progress";
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
-export const Progress = forwardRef<
-  ElementRef<typeof ProgressPrimitive.Root>,
-  ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    className={"relative min-h-4 w-full overflow-hidden rounded-full bg-[#f3f4f6]"}
-    ref={ref}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-[#7c3aed] transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-));
-Progress.displayName = "Progress";
+import { twMerge } from "tailwind-merge";
+
+export const Progress = forwardRef<ElementRef<typeof Root>, ComponentPropsWithoutRef<typeof Root>>(
+  ({ className, value, ...props }, ref) => (
+    <Root
+      className={twMerge("relative h-2 w-full overflow-hidden rounded-full bg-stone-900/20", className)}
+      ref={ref}
+      {...props}
+    >
+      <Indicator
+        className="h-full w-full flex-1 bg-purple-700 transition-all"
+        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      />
+    </Root>
+  ),
+);
+Progress.displayName = Root.displayName;
