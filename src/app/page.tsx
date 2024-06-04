84639b3063f4d9dc6ba33fc4b81b8671a77e6abf
iluaboltov <bogdan.paly1@gmail.com>
2024-06-04 18:55:02 +0300

    feat: Added Dropdown Menu for Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import { mockApi } from "@/api/quiz";
+import { localStorageUtil, mockApi } from "@/api/quiz";
 import { Header } from "@/components/shared/header";
 import { Card } from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { HomeContext } from "@/context/context";
-import { QuizData } from "@/types/questionData";
+import { QuizData } from "@/types/types";
+import { HamburgerMenuIcon, Pencil2Icon, TrashIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -16,373 +23,77 @@ export default function Home() {
   useEffect(() => {
     mockApi().then(res => {
       if (!res) return;
-      const quizDataResult: QuizData[] = JSON.parse(res);
-      setQuiz(quizDataResult);
-      setLoading(false);
+      setQuiz(res);
     });
-    if (window) {
-      window.localStorage.setItem(
-        "quiz",
-        JSON.stringify([
-          {
-            id: 1,
-            questionAndAnswer: [
-              {
-                answer: "C++",
-                id: 1,
-                options: ["ReactJS", "C++", "JavaScript", "CSS"],
-                points: 5,
-                question: "Whch Programming Language is best for writing System Programs?",
-                type: "regular",
-              },
-              {
-                answer: "HTML",
-                id: 2,
-                options: ["Markdown", "Swift", "Kotlin", "HTML"],
-                points: 5,
-                question: "What is the building block of the web?",
-                type: "regular",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 3,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is an Algorithm?",
-                type: "regular",
-              },
-              {
-                answer: ["A list of ToDos", "A programming paradigm", "A step by step by detailed guide"],
-                id: 4,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is?",
-                type: "multi",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 5,
-                points: 5,
-                question: "What is an?",
-                type: "text",
-              },
-            ],
-            title: "Quiz 1",
-          },
-          {
-            id: 2,
-            questionAndAnswer: [
-              {
-                answer: "C++",
-                id: 1,
-                options: ["ReactJS", "C++", "JavaScript", "CSS"],
-                points: 5,
-                question: "Whch Programming Language is best for writing System Programs?",
-                type: "regular",
-              },
-              {
-                answer: "HTML",
-                id: 2,
-                options: ["Markdown", "Swift", "Kotlin", "HTML"],
-                points: 5,
-                question: "What is the building block of the web?",
-                type: "regular",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 3,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is an Algorithm?",
-                type: "regular",
-              },
-              {
-                answer: ["A list of ToDos", "A programming paradigm", "A step by step by detailed guide"],
-                id: 4,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is?",
-                type: "multi",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 5,
-                points: 5,
-                question: "What is an?",
-                type: "text",
-              },
-            ],
-            title: "Quiz 2",
-          },
-          {
-            id: 3,
-            questionAndAnswer: [
-              {
-                answer: "C++",
-                id: 1,
-                options: ["ReactJS", "C++", "JavaScript", "CSS"],
-                points: 5,
-                question: "Whch Programming Language is best for writing System Programs?",
-                type: "regular",
-              },
-              {
-                answer: "HTML",
-                id: 2,
-                options: ["Markdown", "Swift", "Kotlin", "HTML"],
-                points: 5,
-                question: "What is the building block of the web?",
-                type: "regular",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 3,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is an Algorithm?",
-                type: "regular",
-              },
-              {
-                answer: ["A list of ToDos", "A programming paradigm", "A step by step by detailed guide"],
-                id: 4,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is?",
-                type: "multi",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 5,
-                points: 5,
-                question: "What is an?",
-                type: "text",
-              },
-            ],
-            title: "Quiz 3",
-          },
-          {
-            id: 4,
-            questionAndAnswer: [
-              {
-                answer: "C++",
-                id: 1,
-                options: ["ReactJS", "C++", "JavaScript", "CSS"],
-                points: 5,
-                question: "Whch Programming Language is best for writing System Programs?",
-                type: "regular",
-              },
-              {
-                answer: "HTML",
-                id: 2,
-                options: ["Markdown", "Swift", "Kotlin", "HTML"],
-                points: 5,
-                question: "What is the building block of the web?",
-                type: "regular",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 3,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is an Algorithm?",
-                type: "regular",
-              },
-              {
-                answer: ["A list of ToDos", "A programming paradigm", "A step by step by detailed guide"],
-                id: 4,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is?",
-                type: "multi",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 5,
-                points: 5,
-                question: "What is an?",
-                type: "text",
-              },
-            ],
-            title: "Quiz 4",
-          },
-          {
-            id: 5,
-            questionAndAnswer: [
-              {
-                answer: "C++",
-                id: 1,
-                options: ["ReactJS", "C++", "JavaScript", "CSS"],
-                points: 5,
-                question: "Whch Programming Language is best for writing System Programs?",
-                type: "regular",
-              },
-              {
-                answer: "HTML",
-                id: 2,
-                options: ["Markdown", "Swift", "Kotlin", "HTML"],
-                points: 5,
-                question: "What is the building block of the web?",
-                type: "regular",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 3,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is an Algorithm?",
-                type: "regular",
-              },
-              {
-                answer: ["A list of ToDos", "A programming paradigm", "A step by step by detailed guide"],
-                id: 4,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is?",
-                type: "multi",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 5,
-                points: 5,
-                question: "What is an?",
-                type: "text",
-              },
-            ],
-            title: "Quiz 5",
-          },
-          {
-            id: 6,
-            questionAndAnswer: [
-              {
-                answer: "C++",
-                id: 1,
-                options: ["ReactJS", "C++", "JavaScript", "CSS"],
-                points: 5,
-                question: "Whch Programming Language is best for writing System Programs?",
-                type: "regular",
-              },
-              {
-                answer: "HTML",
-                id: 2,
-                options: ["Markdown", "Swift", "Kotlin", "HTML"],
-                points: 5,
-                question: "What is the building block of the web?",
-                type: "regular",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 3,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is an Algorithm?",
-                type: "regular",
-              },
-              {
-                answer: ["A list of ToDos", "A programming paradigm", "A step by step by detailed guide"],
-                id: 4,
-                options: [
-                  "A list of ToDos",
-                  "A programming paradigm",
-                  "A step by step by detailed guide",
-                  "A programming problem",
-                ],
-                points: 5,
-                question: "What is?",
-                type: "multi",
-              },
-              {
-                answer: "A step by step by detailed guide",
-                id: 5,
-                points: 5,
-                question: "What is an?",
-                type: "text",
-              },
-            ],
-            title: "Quiz 6",
-          },
-        ]),
-      );
-    }
+    setLoading(false);
   }, []);
+  const removeQuiz = (indexRemove: number) => {
+    const quizRes = localStorageUtil("quiz").get() as QuizData[];
+    const newQuiz = quizRes.filter((value, index) => {
+      if (indexRemove === index) return;
+      return value;
+    });
+    setQuiz(prevState => {
+      return prevState?.filter((value, index) => {
+        if (indexRemove === index) return;
+        return value;
+      });
+    });
+    localStorageUtil("quiz").delete();
+    localStorageUtil("quiz").set(newQuiz);
+  };
   if (!loading && quiz) {
     return (
       <HomeContext.Provider value={{ isTracking, quiz, setQuiz, setTracking }}>
         <Header />
-        <main className={"flex h-full justify-center gap-4"}>
-          {quiz.map((item, index) => {
-            return (
-              <Link
-                href={`${index}`}
-                key={crypto.randomUUID()}
-                onClick={e => {
-                  if (isTracking) {
-                    e.preventDefault();
-                  }
-                }}
-              >
-                <Card className={`h-64 w-40 pt-6 ${isTracking ? "hover:bg-red-400" : ""}`} id={`quiz-${index + 1}`}>
-                  <div className={"relative h-2/3 w-full rounded-t-lg"}>
-                    <Image alt={"quiz profile"} fill src={"/quiz.avif"} />
+        <main className={"flex h-full flex-wrap justify-center gap-4"}>
+          {!!quiz &&
+            quiz.map((item, index) => {
+              return (
+                <Card
+                  className={`relative h-64 w-40 pt-7 transition-all ease-in-out hover:scale-110 ${isTracking ? "hover:bg-red-400" : ""}`}
+                  id={`quiz-${index + 1}`}
+                  key={index}
+                >
+                  <div className={"absolute right-1 top-1"}>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger
+                        className={"rounded-md border-stone-800 bg-stone-950 p-1 text-sm text-stone-50"}
+                      >
+                        <HamburgerMenuIcon />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        <DropdownMenuItem>
+                          <Link href={`/edit-quiz/${index}`}>
+                            <Pencil2Icon />
+                          </Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            removeQuiz(index);
+                          }}
+                        >
+                          <TrashIcon />
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
                   </div>
-                  <div className={"flex justify-center p-4 text-3xl"}>{item.title}</div>
+                  <Link
+                    href={`${index}`}
+                    onClick={e => {
+                      if (isTracking) {
+                        e.preventDefault();
+                      }
+                    }}
+                  >
+                    <div className={"relative h-2/3 w-full rounded-t-lg"}>
+                      <Image alt={"quiz profile"} fill src={"/quiz.avif"} />
+                    </div>
+                    <div className={"flex justify-center p-4 text-3xl"}>{item.quizTitle}</div>
+                  </Link>
                 </Card>
-              </Link>
-            );
-          })}
+              );
+            })}
         </main>
       </HomeContext.Provider>
     );
