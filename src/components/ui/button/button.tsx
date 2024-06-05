e85ef0bb1a9ae080cc4349504087752781a39660
iluaboltov <bogdan.paly1@gmail.com>
2024-06-05 18:09:40 +0300

    fix: Minor changes

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,16 +1,27 @@
-import React, { ReactNode } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
 
-type ButtonProps = {
-  action: () => void;
-  children?: ReactNode;
-  className: string;
-};
-export const Button = ({ action, children, className = "" }: ButtonProps) => {
-  return (
-    <button className={className} onClick={action} type={"button"}>
-      {children}
-    </button>
-  );
-};
+export type ButtonProps = {
+  asChild?: boolean;
+  onAction?: () => void;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ asChild = false, className, onAction, ...props }, ref) => {
+    const Component = asChild ? Slot : "button";
 
+    return (
+      <div onClick={onAction}>
+        <Component
+          className={twMerge(
+            "min-h-8 w-full rounded-lg bg-purple-600 px-4 py-0.5 text-sm font-medium text-white transition-all ease-in-out hover:bg-purple-800",
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  },
+);
 Button.displayName = "Button";
